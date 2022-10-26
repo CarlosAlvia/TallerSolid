523343cef81c5718deeae6386d810500db84c1f3
CarlosAlvia <cjalvia@espol.edu.ec>
2022-10-26 16:10:55 -0500

    Correcciones paquete postres
    Correcciones paquete postres

--- src/Postres/Postre.java
+++ src/Postres/Postre.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 import Adicionales.Aderezo;
 
 public class Postre {
-	private String sabor;
-	private double precioParcial;
-	private ArrayList<String> aderezos;
+	protected String sabor;
+	protected double precioParcial;
+	protected ArrayList<String> aderezos;
 	
-	public Postre(String sabor, double precioParcial) {
+	public Postre(String sabor) {
 		this.sabor=sabor;
-		this.precioParcial=precioParcial;
 		aderezos=new ArrayList<>();
 	}
 	public ArrayList<String> getAderezos() {
@@ -20,4 +19,20 @@ public class Postre {
 	public void agregarAderezo() {
 		//Este método agrega aderezos
 	}
+	public String getSabor() {
+		return sabor;
+	}
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+	public double getPrecioParcial() {
+		return precioParcial;
+	}
+	public void setPrecioParcial(double precioParcial) {
+		this.precioParcial = precioParcial;
+	}
+	public void setAderezos(ArrayList<String> aderezos) {
+		this.aderezos = aderezos;
+	}
+	
 }
