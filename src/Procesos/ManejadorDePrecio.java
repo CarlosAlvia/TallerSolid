1ceddf45c30ae83754b1a62da5afe57d6ae4fea2
LuisAntonioQuezadaAristega <107262704+LuisAntonioQuezadaAristega@users.noreply.github.com>
2022-10-26 16:15:46 -0500

    se creo la clase ManejadorDePrecio y se movio los metodos calcularPrecioFinal() y  showPrecioFinal()

---
+++ src/Procesos/ManejadorDePrecio.java
@@ -0,0 +1,14 @@
+
+import Postres.Postre;
+
+public class ManejadorDePrecio {
+	public double calcularPrecioFinal() {
+		double precioFinal;
+        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
+        return precioFinal;
+	}
+	
+	public String showPrecioFinal() {
+		return "Precio Final: $ " + calcularPrecioFinal();
+	}
+}
